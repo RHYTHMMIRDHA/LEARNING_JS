018d757c0f0f413ab7b5962cd6bcda977751351f
RHYTHMMIRDHA <rhythmmirdha2005@gmail.com>
2026-05-07 06:43:34 +0000

    datatype conversion learnt

---
+++ 01_BASICS/3_dataTypeConversion.js
@@ -0,0 +1,52 @@
+//      CONVERSION TO Number
+
+let name = "Rhythm";
+let age = "21";
+let city = null;
+let state = undefined;
+
+let nameNum = Number(name);
+console.log(nameNum);
+console.log(typeof nameNum);
+
+let ageNum = Number(age);
+console.log(ageNum);
+console.log(typeof ageNum);
+
+let cityNum = Number(city);
+console.log(cityNum);
+console.log(typeof cityNum);
+
+let stateNum = Number(state);
+console.log(stateNum);
+console.log(typeof stateNum);
+
+// CONVERSION TO Boolean
+
+let one = 1;
+let two = 0;
+let three = "hi";
+let four = "";
+
+let oneBool = Number(one);
+console.log(oneBool);
+console.log(typeof oneBool);
+
+let twoBool = Number(two);
+console.log(twoBool);
+console.log(typeof twoBool);
+
+let threeBool = Number(three);
+console.log(threeBool);
+console.log(typeof threeBool);
+
+let fourBool = Number(four);
+console.log(fourBool);
+console.log(typeof fourBool);
+
+// COVERSION TO STRING
+
+let five = null;
+let fiveString = String(five);
+console.log(fiveString); // null
+console.log(typeof fiveString); // string
