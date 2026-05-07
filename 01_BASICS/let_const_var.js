5fab9716ac8a01b2bcfc1cd333dc1891e242adf1
RHYTHMMIRDHA <rhythmmirdha2005@gmail.com>
2026-05-07 05:05:00 +0000

    var, const, let learnt successfully !!!

---
+++ 01_BASICS/let_const_var.js
@@ -0,0 +1,12 @@
+{
+const name = "Rhythm";
+const Id = "a12";
+
+let state = "Haryana";
+let city = "Narwana";
+
+console.table([name, Id, state, city])
+}
+
+const city = "banglore";
+console.table([city]);
